264b88d0561a920dd2831125a5825722fd5fc13c
Daniel Kiss <littlesnorrboy@gmail.com>
2024-08-08 14:41:55 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "brengin devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs@{ self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        x11libs = with pkgs; [
+          xorg.libxcb
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          libxkbcommon
+          vulkan-loader
+        ];
+      in
+      with pkgs;
+      {
+
+        devShells.default = mkShell {
+          buildInputs = x11libs ++ [
+            # rust deps
+            mold
+            llvmPackages_latest.clang
+            stdenv
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" "rustfmt" ];
+              targets = [ ];
+            })
+            # winit deps          
+            #
+            # To use wayland
+            wayland
+
+            # sound deps
+            alsa-lib
+            pkg-config
+            # tools
+            cargo-nextest
+            cargo-edit
+            just
+            renderdoc
+          ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath ([
+            alsa-lib
+            wayland
+          ] ++ x11libs);
+        };
+      }
+    );
+}
+
