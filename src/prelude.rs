fc7a61c97eb6c9ba076bfecacd2adae17c4ef460
Daniel Kiss <littlesnorrboy@gmail.com>
2024-08-21 10:14:39 +0200

    More preludes

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,2 +1,8 @@
+pub use crate::assets::*;
+pub use crate::transform::{transform_bundle, GlobalTransform, Transform};
+pub use crate::App;
+pub use crate::DeltaTime;
 pub use crate::Plugin;
+pub use crate::Time;
+pub use crate::Timer;
 pub use cecs::prelude::*;
