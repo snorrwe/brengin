36e2f6f2459973fbe0283518f7d250e11cc38e98
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-27 22:16:10 +0200

    Pipeline boilerplate

--- src/ui/core.rs
+++ src/ui/core.rs
@@ -1,5 +1,6 @@
 use crate::renderer::{
-    ExtractionPlugin, RenderCommand, RenderCommandInput, RenderCommandPlugin, RenderPass,
+    ExtractionPlugin, GraphicsState, RenderCommand, RenderCommandInput, RenderCommandPlugin,
+    RenderPass,
 };
 use cecs::prelude::*;
 
@@ -31,16 +32,28 @@ pub struct DrawRect {
     pub color: u32,
 }
 
+struct RectPipeline {}
+impl RectPipeline {
+    fn new(graphics_state: &GraphicsState) -> Self {
+        Self {}
+    }
+}
+
 struct RectRenderCommand;
 
 impl<'a> RenderCommand<'a> for RectRenderCommand {
-    type Parameters = Query<'a, &'static RectRequests>;
+    type Parameters = (Query<'a, &'static RectRequests>, Res<'a, RectPipeline>);
 
     fn render<'r>(input: &'r mut RenderCommandInput<'a>, pipeline: &'r Self::Parameters) {}
 }
 
 fn setup(mut cmd: Commands) {
     cmd.spawn().insert(RectRequests::default());
+}
+
+fn setup_renderer(mut cmd: Commands, graphics_state: Res<GraphicsState>) {
+    let pipeline = RectPipeline::new(&graphics_state);
+    cmd.insert_resource(pipeline);
 }
 
 pub struct UiCorePlugin;
@@ -53,5 +66,8 @@ impl Plugin for UiCorePlugin {
         app.add_plugin(RenderCommandPlugin::<RectRenderCommand>::new(
             RenderPass::Ui,
         ));
+        if let Some(ref mut renderer) = app.render_app {
+            renderer.add_startup_system(setup_renderer);
+        }
     }
 }
