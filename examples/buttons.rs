b5745e284752ab4a1f91885901048e17c75845ca
Daniel Kiss <littlesnorrboy@gmail.com>
2024-10-18 22:14:49 +0200

    Add camera to buttons example

--- examples/buttons.rs
+++ examples/buttons.rs
@@ -1,6 +1,8 @@
-use brengin::prelude::*;
+use brengin::camera::{camera_bundle, PerspectiveCamera, WindowCamera};
 use brengin::ui::builder::Ui;
+use brengin::{prelude::*, transform};
 use brengin::{App, DefaultPlugins};
+use glam::Vec3;
 use tracing::info;
 
 fn buttons_ui(mut ctx: ResMut<Ui>) {
@@ -17,9 +19,26 @@ fn buttons_ui(mut ctx: ResMut<Ui>) {
     });
 }
 
+fn setup(mut cmd: Commands) {
+    //camera
+    cmd.spawn()
+        .insert(WindowCamera)
+        .insert_bundle(camera_bundle(PerspectiveCamera {
+            eye: Vec3::new(0.0, 0.0, 100.0),
+            target: Vec3::ZERO,
+            up: Vec3::Y,
+            aspect: 16.0 / 9.0,
+            fovy: std::f32::consts::TAU / 6.0,
+            znear: 5.0,
+            zfar: 5000.0,
+        }))
+        .insert_bundle(transform_bundle(transform::Transform::default()));
+}
+
 async fn game() {
     let mut app = App::default();
     app.add_plugin(DefaultPlugins);
+    app.add_startup_system(setup);
     app.with_stage(brengin::Stage::Update, |s| {
         s.add_system(buttons_ui);
     });
