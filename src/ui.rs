745dc4bb08109ba2c4b94227da3481ee6a23268b
Daniel Kiss <littlesnorrboy@gmail.com>
2026-01-06 22:36:48 +0100

    feat(ui): Add color parsing utility functions

--- src/ui.rs
+++ src/ui.rs
@@ -12,7 +12,7 @@ pub mod textured_rect_pipeline;
 #[cfg(test)]
 mod tests;
 
-use std::{any::TypeId, collections::HashMap, ptr::NonNull, time::Duration};
+use std::{any::TypeId, collections::HashMap, ptr::NonNull, str::FromStr, time::Duration};
 
 use cecs::{prelude::*, query};
 use glam::IVec2;
@@ -36,13 +36,80 @@ pub struct Color(pub u32);
 
 impl Default for Color {
     fn default() -> Self {
-        Self(0xFF)
+        Self::from_rgb(0)
     }
 }
 
 impl From<u32> for Color {
     fn from(value: u32) -> Self {
         Self::from_rgba(value)
+    }
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum ColorParseError {
+    #[error("Color strings must begin with a hashmark (#)")]
+    MissingHash,
+    #[error("Invalid length of {0}. Expected length 4 (#rgb) or 7 (#rrggbb) or 5 (#rgba) or 9 (#rrggbbaa)")]
+    BadLength(usize),
+    #[error("Invalid character in the color string: {0} Expected hexadecimal number")]
+    InvalidCharacter(char),
+    #[error("Expected an ascii string")]
+    NotAscii,
+}
+
+fn collapse_color(buffer: &mut [u8], l: usize) -> Color {
+    for i in (0..(2 * l)).step_by(2) {
+        buffer[i] = (buffer[i] * 16) + (buffer[i + 1]);
+    }
+    for i in 1..l {
+        buffer[i] = buffer[i * 2];
+    }
+    Color::from_slice(&buffer[..l])
+}
+
+impl FromStr for Color {
+    type Err = ColorParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if !s.starts_with('#') {
+            return Err(ColorParseError::MissingHash);
+        }
+
+        // TODO: assert little endian
+        let mut buffer = [0u8; 8];
+        let mut l = 0;
+        for c in s.chars().skip(1).take(8).map(|x| x.to_ascii_lowercase()) {
+            let x = c as u8;
+            let c = if b'0' <= x && x <= b'9' {
+                x - b'0'
+            } else if b'a' <= x && x <= b'f' {
+                x - b'a' + 10
+            } else {
+                return Err(ColorParseError::InvalidCharacter(c));
+            };
+            buffer[l] = c;
+            l += 1;
+        }
+        match l {
+            3 => {
+                for i in (0..6).step_by(2).rev() {
+                    buffer[i] = buffer[i / 2];
+                    buffer[i + 1] = buffer[i / 2];
+                }
+                Ok(collapse_color(&mut buffer, 3))
+            }
+            4 => {
+                for i in (0..8).step_by(2).rev() {
+                    buffer[i] = buffer[i / 2];
+                    buffer[i + 1] = buffer[i / 2];
+                }
+                Ok(collapse_color(&mut buffer, 4))
+            }
+            6 => Ok(collapse_color(&mut buffer, 3)),
+            8 => Ok(collapse_color(&mut buffer, 4)),
+            _ => Err(ColorParseError::BadLength(l + 1)),
+        }
     }
 }
 
@@ -63,10 +130,14 @@ impl Color {
 
     /// consumes at most 4 items, in order rgba
     pub fn from_slice(bytes: &[u8]) -> Self {
-        let mut res = 0xFF;
+        let mut res = 0;
 
         for (i, b) in bytes.iter().enumerate().take(4) {
-            res |= (*b as u32) << (32 - 8 * i);
+            res |= (*b as u32) << (24 - 8 * i);
+        }
+
+        if bytes.len() < 4 {
+            res |= 0xFF;
         }
 
         // only 1 item has been submitted, splat it
