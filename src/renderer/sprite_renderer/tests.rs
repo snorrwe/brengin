d21c2670f1c772f2e51f8ec78e5d7963eeb25b5a
Daniel Kiss <littlesnorrboy@gmail.com>
2026-04-24 22:15:26 +0200

    Add get_instance_box with padding test

--- src/renderer/sprite_renderer/tests.rs
+++ src/renderer/sprite_renderer/tests.rs
@@ -3,7 +3,7 @@ use glam::Vec2;
 use crate::renderer::sprite_renderer::sprite_sheet::{SpriteInstance, SpriteSheet};
 
 #[test]
-fn test_sprite_sheet_extract() {
+fn test_sprite_sheet_instance_uv() {
     let img = image::RgbImage::new(64, 64);
     let sheet = SpriteSheet::from_grid(Vec2::ZERO, Vec2::splat(32.0), 2, img.into());
 
@@ -24,4 +24,19 @@ fn test_sprite_sheet_extract() {
 
     assert_eq!(uvbox[0], Vec2::new(1.0, 0.5));
     assert_eq!(uvbox[1], Vec2::new(0.5, 1.0));
+}
+
+#[test]
+fn test_sprite_sheet_instance_box_with_padding() {
+    let img = image::RgbImage::new(100, 100);
+    let sheet = SpriteSheet::from_grid(Vec2::new(1.0, 2.0), Vec2::splat(10.0), 10, img.into());
+
+    let uvbox = sheet.get_instance_box(SpriteInstance {
+        index: 0,
+        flip: false,
+        ..Default::default()
+    });
+
+    assert_eq!(uvbox[0], Vec2::new(1.0, 2.0));
+    assert_eq!(uvbox[1], Vec2::new(9.0, 8.0));
 }
