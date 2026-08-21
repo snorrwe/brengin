859a07f27947d222eb7e0da2fb633172ffcdf89f
Daniel Kiss <littlesnorrboy@gmail.com>
2026-08-21 13:30:04 +0200

    Add oneshot channel

---
+++ src/oneshot.rs
@@ -0,0 +1,68 @@
+//! Single use channel
+
+use std::{
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::atomic::{AtomicI8, Ordering},
+};
+
+const NOT_READY: i8 = 0;
+const READY: i8 = 1;
+const WRITING: i8 = 2;
+const READING: i8 = 3;
+
+pub struct OneShot<T> {
+    value: UnsafeCell<MaybeUninit<T>>,
+    status: AtomicI8,
+}
+
+impl<T> Default for OneShot<T> {
+    fn default() -> Self {
+        Self {
+            value: UnsafeCell::new(MaybeUninit::uninit()),
+            status: AtomicI8::new(NOT_READY),
+        }
+    }
+}
+
+impl<T> Drop for OneShot<T> {
+    fn drop(&mut self) {
+        let _ = self.try_receive();
+    }
+}
+
+impl<T> OneShot<T> {
+    pub fn send(&self, value: T) {
+        if self
+            .status
+            .compare_exchange(NOT_READY, WRITING, Ordering::Relaxed, Ordering::Relaxed)
+            .is_err()
+        {
+            panic!("Only one send is supported");
+        }
+        unsafe {
+            let slot = self.value.get().as_mut().unwrap();
+            slot.write(value);
+            self.status.store(READY, Ordering::Release);
+        }
+    }
+
+    pub fn try_receive(&self) -> Option<T> {
+        if self
+            .status
+            .compare_exchange(READY, READING, Ordering::Relaxed, Ordering::Relaxed)
+            .is_err()
+        {
+            return None;
+        }
+        let value = unsafe { self.value.get().as_mut_unchecked().assume_init_read() };
+
+        self.status.store(NOT_READY, Ordering::Release);
+
+        Some(value)
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.status.load(Ordering::Relaxed) == 1
+    }
+}
